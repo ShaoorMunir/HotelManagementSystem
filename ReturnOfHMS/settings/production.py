f5d3d0161364092469ef72b2cd36ba13d4233c2e
Shaoor Munir <shaoormunir@outlook.com>
2018-04-15 21:44:39 +0500

    added postgresql link

--- ReturnOfHMS/settings/production.py
+++ ReturnOfHMS/settings/production.py
@@ -1,5 +1,9 @@
+import dj_database_url
+
 from ReturnOfHMS.settings.common import *
 
 DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
