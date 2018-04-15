b2735ff4a79d5e532c782dcd400467ab00ee8312
Shaoor Munir <shaoormunir@outlook.com>
2018-04-15 17:46:08 +0500

    Initial commit

---
+++ ReturnOfHMS/settings/development.py
@@ -0,0 +1,9 @@
+from ReturnOfHMS.settings.common import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '5!i@@7r60%03+-$(ue4_il-207k&up6-$94v+q6v&hc572x4l-'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
---
+++ ReturnOfHMS/settings/production.py
@@ -0,0 +1,5 @@
+from ReturnOfHMS.settings.common import *
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
