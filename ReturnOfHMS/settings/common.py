79b1d70c12ab400b4210dcbc9671803af5bfab48
Shaoor Munir <shaoormunir@outlook.com>
2018-04-15 18:31:22 +0500

    4 commit

--- ReturnOfHMS/settings/common.py
+++ ReturnOfHMS/settings/common.py
@@ -123,5 +123,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
