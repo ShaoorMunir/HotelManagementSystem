605ac3f3f1f6a5be7fab1c6009a894b6349420f7
Shaoor Munir <shaoormunir@outlook.com>
2018-04-20 11:49:36 +0500

    models

--- HMS/admin.py
+++ HMS/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import Room, RoomType, Registeration, Image, Profile
 
 # Register your models here.
